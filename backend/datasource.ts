9eb79b93e223a829823ced84a5ab148f30d91226
revati junghare <revati.junghare@geta.ai>
2026-01-09 19:05:32 +0530

    add

--- backend/datasource.ts
+++ backend/datasource.ts
@@ -1,15 +1,32 @@
+// import { DataSource } from "typeorm";
+// import { TodoEntity } from "./entities/todo.entity";
+// import { UserEntity } from "./entities/user.entity";
+// import 'dotenv/config'
+
+// export const AppDataSource = new DataSource({
+//     type:'postgres',
+//     host:'localhost',
+//     port: Number(process.env.PORT),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     entities: [TodoEntity,UserEntity],
+//     synchronize:true
+// })
+
+
 import { DataSource } from "typeorm";
 import { TodoEntity } from "./entities/todo.entity";
 import { UserEntity } from "./entities/user.entity";
 import 'dotenv/config'
 
 export const AppDataSource = new DataSource({
-    type:'postgres',
-    host:'localhost',
-    port: Number(process.env.PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [TodoEntity,UserEntity],
-    synchronize:true
-})
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    entities: [TodoEntity, UserEntity],
+    synchronize: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
